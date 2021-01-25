d68c3781e5ae3f38db74b9e8119798c52ccbdcee
trangtpham511 <75021511+trangtpham511@users.noreply.github.com>
2021-01-25 17:00:25 +0000

    Add files via upload

--- lab25Jan.sql
+++ lab25Jan.sql
@@ -1,37 +1,60 @@
-/*1. List number of films per `category`.
-2. Display the first and last names, as well as the address, of each staff member.
-3. Display the total amount rung up by each staff member in August of 2005.
-4. List each film and the number of actors who are listed for that film.
-5. Using the tables `payment` and `customer` and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name.
-*/
--- 1
-select count(f.film_id), fc.category_id, c.name from film f
-join film_category fc 
-on f.film_id = fc.film_id
-join category c on fc.category_id = c.category_id
-group by fc.category_id
-order by category_id;
+/*1. Write a query to display for each store its store ID, city, and country.
+2. Write a query to display how much business, in dollars, each store brought in.
+3. What is the average running time of films by category?
+4. Which film categories are longest?
+5. Display the most frequently rented movies in descending order.
+6. List the top five genres in gross revenue in descending order.
+7. Is "Academy Dinosaur" available for rent from Store 1? */
+use sakila;
+-- 1 
+select s.store_id, c.city, ctr.country from store s
+join address a on s.address_id = a.address_id
+join city c on a.city_id = c.city_id
+join country ctr on c.country_id = ctr.country_id;
 -- 2
-select s.staff_id, s.first_name, s.last_name, a.address from staff s
-join address a
-on s.address_id = a.address_id;
-
+select c.store_id, sum(p.amount) as revenue from customer c
+join payment p on c.customer_id = p.customer_id
+group by c.store_id;
 -- 3
-select
-	count(l.amount) as 'total_payment', s.staff_id, s.first_name, s.last_name from payment as l
-join staff as s
-on l.staff_id = s.staff_id
-where DATE_FORMAT(payment_date, "%M %Y")='August 2005'
-group by staff_id;
--- 4
-select a.film_id, count(a.actor_id) as no_of_actors, f.title from film f
-join film_actor a
-on a.film_id = f.film_id
-group by film_id
-order by film_id;
+use sakila;
+select ca.name as category, round(avg(f.length),2) as average_length from film f
+join film_category fc on f.film_id = fc.film_id
+join category ca on fc.category_id = ca.category_id
+group by fc.category_id;
+-- 4 
+select ca.name as category, round(avg(f.length),2) as average_length from film f
+join film_category fc on f.film_id = fc.film_id
+join category ca on fc.category_id = ca.category_id
+group by fc.category_id
+order by name desc
+limit 1;
 -- 5
-select c.customer_id, c.first_name , c.last_name, sum(p.amount) as total_amount from customer c
-left join payment p
-on c.customer_id = p.customer_id
-group by p.customer_id
-order by c.last_name;
+select f.title, count(r.rental_id) as rental_frequency from rental r
+join inventory i on r.inventory_id = i.inventory_id
+join film f on i.film_id = f.film_id
+group by f.film_id
+order by rental_frequency desc;
+-- 6
+SELECT 
+    SUM(p.amount) AS revenue, c.name AS category
+FROM
+    payment p
+        JOIN
+    rental r ON p.rental_id = r.rental_id
+        JOIN
+    inventory i ON r.inventory_id = i.inventory_id
+        JOIN
+    film_category fc ON i.film_id = fc.film_id
+        JOIN
+    category c ON fc.category_id = c.category_id
+GROUP BY category
+ORDER BY revenue DESC
+LIMIT 5;
+-- 7
+select f.title, r.inventory_id, r.rental_date , r.return_date from rental r 
+join inventory i on r.inventory_id = i.inventory_id
+join film f on i.film_id = f.film_id
+where f.title = 'ACADEMY DINOSAUR'
+order by r.rental_date desc;  
+#as the movie with inventory_id 2,3,7,1,4,8 has been returned, they would be available for rent
+
